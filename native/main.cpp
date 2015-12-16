26236cf482a5ef7ba125c183ada63899bb813dfd
vikas maan-board <maan.vikas@gmail.com>
2015-12-16 10:14:33 +0000

    First commit

---
+++ native/main.cpp
@@ -0,0 +1,125 @@
+/**
+ * @file     : samsung0c.cpp
+ * @brief    :
+ *
+ * @author   : HDX Client Team
+ * @date     : 21-Apr-2015
+ *
+ * Copyright 2015 by Samsung Electronics Inc.
+ *
+ * This software is the confidential and proprietary information
+ * of Samsung Electronics Inc. (Confidential Information).  You
+ * shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with Samsung.
+ *
+ */
+#include "main.h"
+#include <unistd.h>
+#include <signal.h>
+#include <execinfo.h>
+#include <unistd.h>
+#include <string.h>
+
+
+static void window_object_cleared_cb(WebKitWebView  *web_view,WebKitWebFrame *frame,gpointer context,gpointer window_object,gpointer user_data)
+{
+    (void)web_view;
+    (void)frame;
+    (void)window_object;
+    (void)user_data;
+ 
+}
+
+
+
+WebKitWebView *create_gtk_window_around_it(WebKitWebInspector *inspector,WebKitWebView      *webview, void *data)
+{
+	(void)inspector;
+	(void)webview;
+	(void)data;
+	GtkWidget *win;
+	GtkWidget *view;
+	printf("create_gtk_window_around_it\n");
+	win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(win), "Inspector");
+	gtk_window_set_default_size(GTK_WINDOW(win), 600, 400);
+	view = webkit_web_view_new();
+	gtk_container_add(GTK_CONTAINER(win), view);
+	g_object_set_data(G_OBJECT(webview), "inspector-window", win);
+	return WEBKIT_WEB_VIEW(view);
+}
+
+static gboolean show_inspector_window(WebKitWebInspector *inspector,GtkWidget          *webview)
+{
+	(void)inspector;
+	(void)webview;
+	GtkWidget *win;
+	printf("show_inspector_window\n");
+	win = (GtkWidget*)g_object_get_data(G_OBJECT(webview), "inspector-window");
+	gtk_widget_show_all(win);
+	return TRUE;
+}
+
+
+int main(int argc, char* argv[])
+{    
+	std::string t_url="../web/root.html";
+    /* Initialize the widget set */
+    gtk_init (&argc, &argv);    
+
+    //Initialization Flow: Client UI Up (3)
+    /* Create the window widgets */
+    main_window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    GdkScreen* gds = gdk_screen_get_default(); 
+    //gtk_window_set_decorated(GTK_WINDOW(main_window), FALSE);
+    gtk_window_move(GTK_WINDOW(main_window), 0, 0); 
+    // -1, -1 offset due to gtk issue. for full size it consider as fullscreen window and fullscreen samsung0c let top xenapp window to go back
+    //Treating resize request of legacy application as fullscreen request
+    GdkRectangle gRect;
+    gdk_screen_get_monitor_geometry(gds,0,&gRect); //0 refers to primary monitor
+    //gtk_window_resize(GTK_WINDOW(main_window),gdk_screen_get_width(gds),gdk_screen_get_height(gds)-1);        
+    
+    //fixed for RK3288
+    gtk_window_resize(GTK_WINDOW(main_window),gRect.width,gRect.height-1);        
+
+    //gtk_window_fullscreen((GtkWindow*)main_window);
+    //GtkWidget *scrolled_window = gtk_scrolled_window_new (NULL, NULL);
+
+    /* Create the WebKit Web View widget */
+    web_view = webkit_web_view_new ();
+
+    // Redirect all glib logging to boost logging
+    g_log_set_default_handler(GLogHandler, NULL);
+
+    //Initialization Flow: JS Interface Up (4)
+    /* Connect the window object cleared event with callback */
+    g_signal_connect (G_OBJECT (web_view), "window-object-cleared", G_CALLBACK(window_object_cleared_cb), web_view);
+
+    /* Place the WebKitWebView in the GtkScrolledWindow */  
+    gtk_container_add (GTK_CONTAINER (main_window), web_view);
+
+    /* Connect the destroy window event with destroy function */
+    g_signal_connect (G_OBJECT (main_window), "destroy", G_CALLBACK (destroy), Taskbar::Get());
+   
+
+    /* Load our home page*/
+    webkit_web_view_load_uri (WEBKIT_WEB_VIEW (web_view), t_url.c_str());
+
+
+    /* Create the main window */
+    //gtk_window_set_default_size (GTK_WINDOW (main_window), 1024, 600);
+
+    /* Show the application window */
+    gtk_widget_show_all (main_window);
+	
+	
+    //ui thread mail loop
+    gtk_main ();
+
+    //Deinitialization Round
+    //memory cleanup
+
+    return 0;
+}
+
---
+++ native/main.h
@@ -0,0 +1,17 @@
+
+#ifndef _I_MEDIA_EXPLORER_H_
+#define _I_MEDIA_EXPLORER_H_
+
+
+//includes
+#include <stdlib.h>
+#include <string.h>
+#include <gtk/gtk.h>
+#include <glib.h>
+#include <pthread.h>    /* POSIX Threads */
+
+//defines
+//#define ROOT_PATH "file:///%s/site.samsung0c/root.html"
+#define WEB_ROOT "../clientui/web"
+
+#endif //_I_MEDIA_EXPLORER_H_
